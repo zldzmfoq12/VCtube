9b87dda3be55d25cccdcc9c0bee7ff5ece691db1
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-04-08 22:06:12 +0900

    aacc

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="audio and caption crawler", # Replace with your own username
+    version="0.0.1",
+    author="Seugnhun Jeong",
+    author_email="zldzmfoq12@naver.com",
+    description="A pakage for crawling audio and caption from Youtube",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zldzmfoq12/aud_crawler",
+    download_url="https://github.com/zldzmfoq12/aud_crawler/archive/0.0.tar.gz",
+    keywords=['aud_crawler'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
