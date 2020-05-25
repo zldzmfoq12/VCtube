89c79584820f9622d2bc309b41c5444f9b0a9187
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-05-25 15:59:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="vctube", # Replace with your own username
-    version="1.1",
+    version="1.2",
     author="Seugnhun Jeong",
     author_email="zldzmfoq12@naver.com",
     description="A pakage for crawling and processing audio, caption from Youtube",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/zldzmfoq12/aud_crawler",
-    download_url="https://github.com/zldzmfoq12/aud_crawler/archive/1.1.tar.gz",
+    url="https://github.com/zldzmfoq12/aud-crawler",
+    download_url="https://github.com/zldzmfoq12/aud-crawler/archive/1.2.tar.gz",
     keywords=['aud_crawler'],
     packages=setuptools.find_packages(),
     classifiers=[
